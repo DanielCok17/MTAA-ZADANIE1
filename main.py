2f3f69908d6c0e199c1e1665784a82975dda05bf
DanielCok17 <cokydano@gmail.com>
2022-02-27 00:58:16 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#   prevzaté z -> https://github.com/tirfil/PySipFullProxy
+
+import sipfullproxy
+ 
+    
+def main():
+    print("Hello World!")
+    # TODO
+
+if __name__ == "__main__":
+    main()  
+    
+    
