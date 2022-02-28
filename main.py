4408cd766ff01eb8f9292d350ad04d8a92bf811a
DanielCok17 <cokydano@gmail.com>
2022-02-28 14:41:08 +0100

    some other changes

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-#   prevzaté z -> https://github.com/tirfil/PySipFullProxy
+#prevzaté z -> https://github.com/tirfil/PySipFullProxy
 
-from email import message
 import sipfullproxy
 import socketserver 
 import socket
-import logging
-
-# global dictionnary
-recordroute = ""
-topvia = ""
-registrar = {}
- 
     
 def main():
     sipfullproxy.logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',filename='dennik_hovorov.log',level=sipfullproxy.logging.INFO,datefmt='%H:%M:%S')
@@ -18,13 +10,12 @@ def main():
     hostname = socket.gethostname()
     sipfullproxy.logging.info(hostname)
     ipaddress = socket.gethostbyname(hostname)
-    print("Proxy server started at <%s:%s>" % (ipaddress, sipfullproxy.PORT))
+    print("Proxy server started at " ,ipaddress ," ip address and " , sipfullproxy.PORT ,"port")
     sipfullproxy.logging.info(ipaddress)
     sipfullproxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress,sipfullproxy.PORT)
     sipfullproxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress,sipfullproxy.PORT)
     server = socketserver.UDPServer((sipfullproxy.HOST, sipfullproxy.PORT), sipfullproxy.UDPHandler)
     server.serve_forever()
-    # TODO
 
 if __name__ == "__main__":
     main() 
