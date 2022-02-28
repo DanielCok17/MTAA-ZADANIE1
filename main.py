17229e6d83e0a3d73fb1aa1c42e88825cd2e2ac0
Daniel Cok <cok@asdata.sk>
2022-02-28 15:59:55 +0100

    own function in main

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ myPort = sipfullproxy.PORT
 myHost = sipfullproxy.HOST
 myUdpHandler = sipfullproxy.UDPHandler
 myFileName = 'dennik_hovorov.log'
+myDateFormat = '%H:%M:%S'
+myDateString = "%a, %d %b %Y %H:%M:%S "
+myRecordRoute = "Record-Route: <sip:%s:%d;lr>"
+myTopovia = "Via: SIP/2.0/UDP %s:%d"
+myLogFormat = '%(asctime)s:%(levelname)s:%(message)s'
 myHostName = ""
 myIpAddress = ""
 
@@ -16,33 +21,29 @@ def set_host_name():
 
 def set_ipaddress():
     return socket.gethostbyname(set_host_name())
-
-def logging(myIpAddress : str) -> str:
-    sipfullproxy.logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',filename = myFileName,level=sipfullproxy.logging.INFO,datefmt='%H:%M:%S')
-    sipfullproxy.logging.info(sipfullproxy.time.strftime("%a, %d %b %Y %H:%M:%S ", sipfullproxy.time.localtime()))
-    sipfullproxy.logging.info(myIpAddress)
     
 def print_function(ipaddress : str)-> str:
-    print("Proxy server started at " ,ipaddress ," ip address and " , myPort ,"port")   
-                
-
+    print("Proxy server started at " ,ipaddress ," ip address and " , myPort ,"port")  
+    
+def set_localtime():
+    return sipfullproxy.time.localtime()    
 
 def main():
     myHostName = set_host_name()
     myIpAddress = set_ipaddress()
     
-    sipfullproxy.logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',filename = myFileName,level=sipfullproxy.logging.INFO,datefmt='%H:%M:%S')
-    sipfullproxy.logging.info(sipfullproxy.time.strftime("%a, %d %b %Y %H:%M:%S ", sipfullproxy.time.localtime()))
+    sipfullproxy.logging.basicConfig(format=myLogFormat,filename = myFileName,level=sipfullproxy.logging.INFO,datefmt = myDateFormat)
+    sipfullproxy.logging.info(sipfullproxy.time.strftime(myDateString, set_localtime()))
     sipfullproxy.logging.info(myHostName)
     sipfullproxy.logging.info(myIpAddress)
-    #logging(myIpAddress)
+
     print_function(myIpAddress)
     
-    sipfullproxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (myIpAddress,myPort)
-    sipfullproxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (myIpAddress,myPort)
+    sipfullproxy.recordroute = myRecordRoute % (myIpAddress,myPort)
+    sipfullproxy.topvia = myTopovia % (myIpAddress,myPort)
     
-    server = socketserver.UDPServer((myHost, myPort), myUdpHandler)
-    server.serve_forever()
+    myserver = socketserver.UDPServer((myHost, myPort), myUdpHandler)
+    myserver.serve_forever()
 
 if __name__ == "__main__":
     main() 
