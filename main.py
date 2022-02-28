b44d424cac1bfc3257f6e335d83bdc3c5529e0cf
Daniel Cok <cok@asdata.sk>
2022-02-28 15:42:37 +0100

    created own function

--- main.py
+++ main.py
@@ -8,18 +8,39 @@ myPort = sipfullproxy.PORT
 myHost = sipfullproxy.HOST
 myUdpHandler = sipfullproxy.UDPHandler
 myFileName = 'dennik_hovorov.log'
-    
+myHostName = ""
+myIpAddress = ""
 
-def main():
+def set_host_name():
+    return socket.gethostname() 
+
+def set_ipaddress():
+    return socket.gethostbyname(set_host_name())
+
+def logging(myIpAddress : str) -> str:
     sipfullproxy.logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',filename = myFileName,level=sipfullproxy.logging.INFO,datefmt='%H:%M:%S')
     sipfullproxy.logging.info(sipfullproxy.time.strftime("%a, %d %b %Y %H:%M:%S ", sipfullproxy.time.localtime()))
-    hostname = socket.gethostname()
-    sipfullproxy.logging.info(hostname)
-    ipaddress = socket.gethostbyname(hostname)
-    print("Proxy server started at " ,ipaddress ," ip address and " , myPort ,"port")
-    sipfullproxy.logging.info(ipaddress)
-    sipfullproxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress,myPort)
-    sipfullproxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress,myPort)
+    sipfullproxy.logging.info(myIpAddress)
+    
+def print_function(ipaddress : str)-> str:
+    print("Proxy server started at " ,ipaddress ," ip address and " , myPort ,"port")   
+                
+
+
+def main():
+    myHostName = set_host_name()
+    myIpAddress = set_ipaddress()
+    
+    sipfullproxy.logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',filename = myFileName,level=sipfullproxy.logging.INFO,datefmt='%H:%M:%S')
+    sipfullproxy.logging.info(sipfullproxy.time.strftime("%a, %d %b %Y %H:%M:%S ", sipfullproxy.time.localtime()))
+    sipfullproxy.logging.info(myHostName)
+    sipfullproxy.logging.info(myIpAddress)
+    #logging(myIpAddress)
+    print_function(myIpAddress)
+    
+    sipfullproxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (myIpAddress,myPort)
+    sipfullproxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (myIpAddress,myPort)
+    
     server = socketserver.UDPServer((myHost, myPort), myUdpHandler)
     server.serve_forever()
 
