cd2d6880c4c174c45edb41896825d7a6f9f8f089
DanielCok17 <cokydano@gmail.com>
2022-02-28 15:13:54 +0100

    changed main

--- main.py
+++ main.py
@@ -3,18 +3,24 @@
 import sipfullproxy
 import socketserver 
 import socket
+
+myPort = sipfullproxy.PORT
+myHost = sipfullproxy.HOST
+myUdpHandler = sipfullproxy.UDPHandler
+myFileName = 'dennik_hovorov.log'
     
+
 def main():
-    sipfullproxy.logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',filename='dennik_hovorov.log',level=sipfullproxy.logging.INFO,datefmt='%H:%M:%S')
+    sipfullproxy.logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',filename = myFileName,level=sipfullproxy.logging.INFO,datefmt='%H:%M:%S')
     sipfullproxy.logging.info(sipfullproxy.time.strftime("%a, %d %b %Y %H:%M:%S ", sipfullproxy.time.localtime()))
     hostname = socket.gethostname()
     sipfullproxy.logging.info(hostname)
     ipaddress = socket.gethostbyname(hostname)
-    print("Proxy server started at " ,ipaddress ," ip address and " , sipfullproxy.PORT ,"port")
+    print("Proxy server started at " ,ipaddress ," ip address and " , myPort ,"port")
     sipfullproxy.logging.info(ipaddress)
-    sipfullproxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress,sipfullproxy.PORT)
-    sipfullproxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress,sipfullproxy.PORT)
-    server = socketserver.UDPServer((sipfullproxy.HOST, sipfullproxy.PORT), sipfullproxy.UDPHandler)
+    sipfullproxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress,myPort)
+    sipfullproxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress,myPort)
+    server = socketserver.UDPServer((myHost, myPort), myUdpHandler)
     server.serve_forever()
 
 if __name__ == "__main__":
